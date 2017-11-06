c070c723c279960a873b42097a104835bb24351e
sinmorampudi <32035996+sinmorampudi@users.noreply.github.com>
2017-11-06 13:07:17 -0500

    Update Subject.java

--- src/edu/fsu/csc7400/hw8/observer/weather/Subject.java
+++ src/edu/fsu/csc7400/hw8/observer/weather/Subject.java
@@ -2,17 +2,27 @@
  * Class: Object-Oriented Design and Analysis
  * Professor: Orlando Montalvo
  * Assignment: HW 8
- * 
+ * @author Sindhuja Morampudi,Divya Kasu
+ * @version 1.0
  * Date: 2017-10-20
  * 
  * Code taken from HeadFirst Object-Oriented Design Patterns sample
  */
 package edu.fsu.csc7400.hw8.observer.weather;
 
+import java.util.ArrayList;
+
 /**
  * Simple subject/observable class
  */
-public interface Subject {
+public abstract class Subject {
+	
+	/**
+	 * Default constructor
+	 */
+	public Subject() {
+		observers = new ArrayList<Observer>();
+	}
 	/**
 	 * Registers an observer
 	 * 
@@ -20,7 +30,9 @@ public interface Subject {
 	 * 
 	 * @precondition o != null
 	 */
-	public void registerObserver(Observer o);
+	public void registerObserver(Observer o) {
+		observers.add(o);
+	}
 	
 	/**
 	 * Removes a registered observer
@@ -29,10 +41,22 @@ public interface Subject {
 	 * 
 	 * @precondition o != null
 	 */
-	public void removeObserver(Observer o);
+	public void removeObserver(Observer o) {
+		int i = observers.indexOf(o);
+		if (i >= 0) {
+			observers.remove(i);
+		}
+	}
 	
 	/**
 	 * Notifies all registered observers
 	 */
-	public void notifyObservers();
+	public void notifyObservers() {
+		for (int i = 0; i < observers.size(); i++) {
+			Observer observer = (Observer)observers.get(i);
+			observer.update(this);
+		}
+	}
+	
+	private ArrayList<Observer> observers;
 }
