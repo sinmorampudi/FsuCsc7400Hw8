a10273721c18f8d66fc496008c258d2860c59fb0
sinmorampudi <32035996+sinmorampudi@users.noreply.github.com>
2017-11-06 13:04:28 -0500

    Update Observer.java

--- src/edu/fsu/csc7400/hw8/observer/weather/Observer.java
+++ src/edu/fsu/csc7400/hw8/observer/weather/Observer.java
@@ -2,7 +2,8 @@
  * Class: Object-Oriented Design and Analysis
  * Professor: Orlando Montalvo
  * Assignment: HW 8
- * 
+ * @author Sindhuja Morampudi,Divya Kasu
+ * @version 1.0
  * Date: 2017-10-20
  * 
  * Code taken from HeadFirst Object-Oriented Design Patterns sample
@@ -13,14 +14,31 @@ package edu.fsu.csc7400.hw8.observer.weather;
  * Simple observer for weather data
  *
  */
-public interface Observer {
+public abstract class Observer {
+	
+	
+	/**
+	 * Constructor with Subject
+	 * 
+	 * @param Subject
+	 *            the data for initialization
+	 * 
+	 * @precondition weatherData != null which is received in subject 
+	 * parameter here
+	 * @postcondition this will be registered as an observer to subject
+	 */
+	public Observer(Subject subject)
+	{
+		subject.registerObserver(this);
+	}
 	
 	/**
 	 * Updates the temperature, humidity, and pressure of the observer as necessary
 	 * 
-	 * @param temp current temperature
-	 * @param humidity current humidity level
-	 * @param pressure current barometric pressure
+	 * @param Subject Object
 	 */
-	public void update(float temp, float humidity, float pressure);
+	
+	public abstract void update(Subject subject);   
+ 
+	
 }
